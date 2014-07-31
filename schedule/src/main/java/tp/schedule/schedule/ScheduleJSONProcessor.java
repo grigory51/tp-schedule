64e51f10872874b34e0ca22b727fffe4c5474408
Grigory Ozhegov <grigory@ozhegov.name>
2014-07-31 04:38:31 +0400

    сменилась суть

--- schedule/src/main/java/tp/schedule/schedule/ScheduleJSONProcessor.java
+++ schedule/src/main/java/tp/schedule/schedule/ScheduleJSONProcessor.java
@@ -1,36 +1,35 @@
 package tp.schedule.schedule;
 
-import org.json.JSONArray;
+import android.util.Log;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
-import java.util.List;
+import java.util.Comparator;
 
-import tp.schedule.utils.Filter;
 
 /**
  * author: grigory51
  * date: 04.07.14
  */
-public class ScheduleItemFactory {
-    private JSONObject auditories, types, groups, disciplines, places;
+public class ScheduleJSONProcessor {
+    private JSONObject auditories, types, subgroups, disciplines, places;
     private JSONObject timetable;
 
-    public ScheduleItemFactory(JSONObject json) throws JSONException {
+    public ScheduleJSONProcessor(JSONObject json) throws JSONException {
         this.auditories = json.getJSONObject("auditoriums");
         this.places = json.getJSONObject("places");
         this.types = json.getJSONObject("types");
-        this.groups = json.getJSONObject("groups");
+        this.subgroups = json.getJSONObject("groups");
         this.disciplines = json.getJSONObject("disciplines");
         this.timetable = json.getJSONObject("timetable");
     }
 
     public ArrayList<ScheduleItem> getScheduleItems() throws JSONException {
         ArrayList<ScheduleItem> result = new ArrayList<ScheduleItem>();
-
 
         Iterator<?> keys = this.timetable.keys();
         while (keys.hasNext()) {
@@ -46,22 +45,36 @@ public class ScheduleItemFactory {
                 title = item.getString("title");
 
                 String placeId = item.getString("place");
-                String auditoriumId = item.getString("place");
+                String auditoriumId = item.getString("auditorium");
                 if (!placeId.equals("0")) {
                     placeTitle = this.places.getString(placeId);
                 } else if (!auditoriumId.equals("0")) {
                     placeTitle = this.auditories.getString(auditoriumId);
                 }
             } else if (eventType.equals("lesson")) {
-                String disciplineId = item.getString("discipline");
-                title = this.disciplines.getString(disciplineId);
-                String auditoriumId = item.getString("place");
+                title = this.disciplines.getJSONObject(item.getString("discipline")).getString("long_name");
+                String auditoriumId = item.getString("auditorium");
                 if (!auditoriumId.equals("0")) {
                     placeTitle = this.auditories.getString(auditoriumId);
                 }
             }
             result.add(new ScheduleItem(timeStart, timeEnd, title, placeTitle, new String[]{}, eventType));
         }
+
+        Collections.sort(result, new Comparator<ScheduleItem>() {
+            public int compare(ScheduleItem a, ScheduleItem b) {
+                if (a.getTimeStart() == b.getTimeStart()) {
+                    return 0;
+                } else {
+                    return a.getTimeStart() - b.getTimeStart() > 0 ? 1 : -1;
+                }
+            }
+        });
+
         return result;
+    }
+
+    public ScheduleFilter getScheduleFilter() throws JSONException {
+        return new ScheduleFilter(this.disciplines, this.types, this.subgroups);
     }
 }
