f3f02805945ad080d50c8da4b8fe74ec2117177e
Grigory Ozhegov <grigory@ozhegov.name>
2014-09-28 04:32:05 +0400

    асинтаски

---
+++ schedule/src/main/java/ru/mail/tp/schedule/tasks/Task.java
@@ -0,0 +1,10 @@
+package ru.mail.tp.schedule.tasks;
+
+import android.os.AsyncTask;
+
+/**
+ * author: grigory51
+ * date: 26/09/14
+ */
+abstract public class Task extends AsyncTask<Void, Void, TaskResult> {
+}
