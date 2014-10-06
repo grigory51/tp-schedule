19e5ff6b1a983cada6a044a6082e86bbec2dfcf7
Grigory Ozhegov <grigory@ozhegov.name>
2014-10-07 00:52:07 +0400

    Переход к новой структуре фильтров

--- schedule/src/main/java/ru/mail/tp/schedule/schedule/filter/FilterSpinnerItemsContainer.java
+++ schedule/src/main/java/ru/mail/tp/schedule/schedule/filter/FilterSpinnerItemsContainer.java
@@ -1,7 +1,5 @@
 package ru.mail.tp.schedule.schedule.filter;
 
-import org.json.JSONException;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -12,14 +10,14 @@ import ru.mail.tp.schedule.schedule.entities.LessonType;
 import ru.mail.tp.schedule.schedule.entities.Subgroup;
 
 public class FilterSpinnerItemsContainer implements Serializable {
-    private ArrayList<Subgroup> subgroupItems;
-    private ArrayList<Discipline> disciplineItems;
-    private ArrayList<LessonType> lessonTypeItems;
+    private FilterSpinnerList subgroupItems;
+    private FilterSpinnerList disciplineItems;
+    private FilterSpinnerList lessonTypeItems;
 
     private int subgroupPosition, disciplinePosition, lessonTypePosition;
     private boolean showPassed;
 
-    public FilterSpinnerItemsContainer(ArrayList<Subgroup> subgroupItems, ArrayList<Discipline> disciplineItems, ArrayList<LessonType> lessonTypeItems) throws JSONException {
+    public FilterSpinnerItemsContainer(ArrayList<IFilterSpinner> subgroupItems, ArrayList<IFilterSpinner> disciplineItems, ArrayList<IFilterSpinner> lessonTypeItems) {
         Comparator<FilterSpinner> comparator = new Comparator<FilterSpinner>() {
             public int compare(FilterSpinner a, FilterSpinner b) {
                 if (a.getId() == 0) {
@@ -28,17 +26,17 @@ public class FilterSpinnerItemsContainer implements Serializable {
                 if (b.getId() == 0) {
                     return 1;
                 }
-                return a.getTitle().compareTo(b.getTitle());
+                return a.toString().compareTo(b.toString());
             }
         };
 
-        this.subgroupItems = subgroupItems;
-        this.disciplineItems = disciplineItems;
-        this.lessonTypeItems = lessonTypeItems;
+        this.subgroupItems = FilterSpinnerList.createFromArrayList(subgroupItems);
+        this.disciplineItems = FilterSpinnerList.createFromArrayList(disciplineItems);
+        this.lessonTypeItems = FilterSpinnerList.createFromArrayList(lessonTypeItems);
 
-        this.subgroupItems.add(new Subgroup(0, "Все"));
-        this.disciplineItems.add(new Discipline(0, "Все"));
-        this.lessonTypeItems.add(new LessonType(0, "Все"));
+        this.subgroupItems.add(new FilterSpinner(new Subgroup(0, "Все")));
+        this.disciplineItems.add(new FilterSpinner(new Discipline(0, "Все")));
+        this.lessonTypeItems.add(new FilterSpinner(new LessonType(0, "Все")));
 
         Collections.sort(this.subgroupItems, comparator);
         Collections.sort(this.disciplineItems, comparator);
@@ -48,15 +46,15 @@ public class FilterSpinnerItemsContainer implements Serializable {
         this.showPassed = false;
     }
 
-    public ArrayList<Subgroup> getSubgroupItems() {
+    public ArrayList<FilterSpinner> getSubgroupItems() {
         return this.subgroupItems;
     }
 
-    public ArrayList<Discipline> getDisciplineItems() {
+    public ArrayList<FilterSpinner> getDisciplineItems() {
         return this.disciplineItems;
     }
 
-    public ArrayList<LessonType> getLessonTypeItems() {
+    public ArrayList<FilterSpinner> getLessonTypeItems() {
         return this.lessonTypeItems;
     }
 
@@ -76,15 +74,19 @@ public class FilterSpinnerItemsContainer implements Serializable {
         this.disciplinePosition = disciplinePosition;
     }
 
-    public boolean isShowPassed() {
-        return this.showPassed;
-    }
-
     public int getLessonTypePosition() {
         return this.lessonTypePosition;
     }
 
     public void setLessonTypePosition(int lessonTypePosition) {
         this.lessonTypePosition = lessonTypePosition;
+    }
+
+    public boolean isShowPassed() {
+        return this.showPassed;
+    }
+
+    public void setShowPassed(boolean showPassed) {
+        this.showPassed = showPassed;
     }
 }
